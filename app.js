8638deb1b55e848a077910a0d1345e836e838703
admin <admin@MAC-9852-0000.local>
2019-06-24 16:14:22 +0900

    이름(나이) 필터 추가

--- app.js
+++ app.js
@@ -12,23 +12,20 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_, res) => {
-    // fs.readdir('./public/images', function(err, filelist){
-    //     if(err){
-    //         res.send(err);
-    //     }else{
-    //         res.render('index.ejs', { fileList : JSON.stringify(filelist) });
-    //     }
-    // })
     AWS.config.accessKeyId = "AKIAIXBW7VDZ4WIBEWFA";
     AWS.config.secretAccessKey = "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";
     AWS.config.region = 'ap-northeast-2';
 
-    let s3 = new AWS.S3();
+    var s3 = new AWS.S3();
     s3.listObjects({ Bucket : 'survey.image' }).on('success', function handlePage(response) {
-        let fileList = [];
+        var fileList = [];
         for(var name in response.data.Contents){
             fileList.push(response.data.Contents[name].Key.split('/')[1]);
         }
+        // var temp = [];
+        // for(let i = 0; i < 100; i ++ ){
+        //     temp.push(fileList[i]);
+        // }
         res.render('index.ejs', { fileList : JSON.stringify(fileList) });
         // console.log(response.data.Contents);
         // if (response.hasNextPage()) {
